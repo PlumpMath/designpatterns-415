abacc187b90904c5b685bf67724fe89b25462c51
Joel Ferreira Barbosa <u197676@marquise.com.br>
2016-11-25 18:33:42 -0200

    template method

--- template_method/main.rb
+++ template_method/main.rb
@@ -1,9 +1,13 @@
 $LOAD_PATH << '.'
 
 require 'html_report'
+require 'plain_text_report'
 
 class Main
-  report = HtmlReport.new
-  report.output_report		
+  # report = HtmlReport.new
+  # report.output_report		
+
+  report = PlainTextReport.new
+  report.output_report  
 	
 end
