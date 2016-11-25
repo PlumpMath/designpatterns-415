4add971148f443578999fe25022099bc9f966e2d
Joel Ferreira Barbosa <u197676@marquise.com.br>
2016-11-25 19:36:30 -0200

    template method

--- template_method/main.rb
+++ template_method/main.rb
@@ -4,10 +4,10 @@ require 'html_report'
 require 'plain_text_report'
 
 class Main
-  # report = HtmlReport.new
-  # report.output_report		
+  report = HtmlReport.new
+  report.output_report		
 
-  report = PlainTextReport.new
-  report.output_report  
+  # report = PlainTextReport.new
+  # report.output_report  
 	
 end
--- template_method/report.rb
+++ template_method/report.rb
@@ -7,9 +7,9 @@ class Report
   def output_report  	
   	output_start
   	output_head
-  	output_body_start
-  	output_body
-  	output_body_end
+  	@text.each do |line|
+      output_line(line)
+    end
   	output_end
   end
 
@@ -19,24 +19,24 @@ class Report
   	end
   end
   
-  def output_start
-  	raise 'Called abstract method: output_start'
+  def output_start  	
   end
 
   def output_head
-  	raise 'Called abstract method: output_head'
+  	output_line(@title)
   end
 
-  def output_body_start
-  	raise 'Called abstract method: output_body_start'
+  def output_body_start  	
   end
 
-  def output_body_end
-  	raise 'Called abstract method: output_body_end'
+  def output_line(line)
+    raise 'Called abstract method: output_line'  
   end
 
-  def output_end
-  	raise 'Called abstract method: output_end'
+  def output_body_end  	
+  end
+
+  def output_end  	
   end
 
 end
